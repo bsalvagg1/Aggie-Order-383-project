58fd018ce1d606c092c51d041a2e041930dfc50a
bsalvagg1 <120699259+bsalvagg1@users.noreply.github.com>
2024-04-11 21:02:55 -0500

    test

--- aggieorder/Homepage.Designer.vb
+++ aggieorder/Homepage.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Homepage
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -33,7 +34,7 @@ Partial Class Homepage
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(336, 48)
+        Me.Label1.Size = New System.Drawing.Size(269, 38)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "AggieOrder.com"
         '
@@ -55,11 +56,20 @@ Partial Class Homepage
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Account"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(490, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(81, 39)
+        Me.Button1.TabIndex = 5
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Homepage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
@@ -73,4 +83,5 @@ Partial Class Homepage
     Friend WithEvents Label1 As Label
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
 End Class
